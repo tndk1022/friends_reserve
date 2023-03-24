49fd0e8ee41e8421b8d3cffed73db0aab5993427
ksj <tndk102222@gmail.com>
2023-03-24 11:09:14 +0900

    123

---
+++ src/main/java/com/ksj/friends/Reserve1Application.java
@@ -0,0 +1,13 @@
+package com.ksj.friends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reserve1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reserve1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ksj/friends/Reserve1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksj.friends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reserve1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
